a31cd6148ee361953ef79f58d236c70cf3832832
Ahmad Safar <ahmadsafar88@gmail.com>
2019-10-03 03:45:24 +0800

    Formatting JS.

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,21 +4,20 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require('./bootstrap')
 
-window.Vue = require('vue');
+window.Vue = require('vue')
 
-require('jquery-migrate');
-require('owl.carousel');
-require('jquery-sticky');
-require('jquery-waypoints/waypoints.min');
-require('jquery.animate-number');
-require('@fancyapps/fancybox');
-require('jquery.easing');
-require('bootstrap-datepicker');
-window.AOS = require('./noxen/aos');
-require('./noxen/main');
-
+require('jquery-migrate')
+require('owl.carousel')
+require('jquery-sticky')
+require('jquery-waypoints/waypoints.min')
+require('jquery.animate-number')
+require('@fancyapps/fancybox')
+require('jquery.easing')
+require('bootstrap-datepicker')
+window.AOS = require('./noxen/aos')
+require('./noxen/main')
 
 /**
  * The following block of code may be used to automatically register your
@@ -31,7 +30,7 @@ require('./noxen/main');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('example-component', require('./components/ExampleComponent.vue').default)
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -41,4 +40,4 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
-});
+})
